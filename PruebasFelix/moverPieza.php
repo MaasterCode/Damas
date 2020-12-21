30731119c983833f04f25282b602c31851981c99
Felix <felix2mil@gmail.com>
2020-12-21 15:06:45 +0100

    Añadida la carpeta Pruebas Felix

---
+++ PruebasFelix/moverPieza.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<pre>
+    <?php
+        $tablero = array(array(" ", " ", " "), array(" ", " ", " "), array(" ", " ", " "));        
+       
+        function montaTablero(){
+            global $tablero;
+            for($i = 0; $i < count($tablero); $i++){
+                for($j = 0; $j < count($tablero[$i]); $j++){
+                    if(($i + $j)%2 == 0){
+                        $tablero[$i][$j] = 'x';
+                    }else{
+                        $tablero[$i][$j] = ' ';
+                    }
+
+                }
+            }    
+        }
+        function moverFicha($posIniX, $posIniY, $posFinX, $posFinY){
+            global $tablero;
+            $tablero[$posIniX][$posIniY] = " ";
+            $tablero[$posFinX][$posFinY] = "x";
+        }
+        montaTablero();
+        var_dump($tablero);
+    ?>
+    <p>Mueve</p>
+    <?php
+        moverFicha(0, 0, 0, 1);
+        var_dump($tablero);
+    ?>
+</pre>
+</body>
+</html>
--- damas.php
+++ damas.php
@@ -12,20 +12,34 @@
             display: grid;
             grid-template-columns: repeat(10, 1fr);
             grid-auto-rows: 1fr;
-            width: 700px;
-            height: 700px;
+            width: 450px;
+            height: 450px;
         }
 
         .tablero div {
             border: 1px solid black;
+            width: 45px;
+            height: 45px;
+
         }
 
-        .tabalero:nth-child(odd) {
-            background: black;
+        .tablero div:nth-child(2n) {
+            background: grey;
+            z-index: -1;
         }
 
-        .tablero:nth-child(even) {
-            background: white;
+        p{
+            position: absolute;
+        }
+
+        .par{
+            position: absolute;
+            right: 50px;
+        }
+
+        .par:nth-of-type(even){
+            bottom: 100px;
+
         }
     </style>
 </head>
@@ -37,11 +51,14 @@
     class Tablero
     {
         public $casillas;
+        public $piezas;
 
         function __construct()
         {
             $this->casillas = array();
             $this->casillas = array_fill(0, 10, array());
+            $this->blancas = array();
+            $this->negras = array();
         }
 
         //Para rellenar el tablero en el orden de la y
@@ -68,6 +85,10 @@
             return $this->casillas[$posX][$posY];
         }
 
+        function getPiezas(){
+            return $this->negras;
+        }
+
         //Es un bucle anidado para crear en el array de tablero las casillas
 
         function rellenaTableroDeCasillas()
@@ -79,7 +100,27 @@
             }
         }
 
+        function crearFichas(){
+            // for ($i = 0; $i < 3; $i++) {
+            //     // ($i % 2) == 0 ? $j = 0: $j = 1;
+            //     for ($j=0; $j < 5; $j+2) {
+                    
+                        // $this->negras[$i][$j] = new Pieza($i, $j, 'negro');
+                    
+            //     }
+            // }
+
+        //     for ($i = count($this->casillas) - 1; $i > count($this->casillas)-4; $i--) {
+        //         for ($j = $i%2; $j < 5 - $i%2; $j+2) {
+                    
+        //                 $this->negras[$i][$j] = new Pieza($i, $j, 'blanco');
+                    
+        //         }
+        //     }
+        }
+        
         //Rellena el tablero de fichas, esto solo se hace inicialmente
+
 
         function rellenaTableroDeFichas()
         {
@@ -88,18 +129,16 @@
 
             for ($i = 0; $i < 3; $i++) {
                 for ($j = 0; $j < count($this->casillas[$i]); $j++) {
-                    if (($i + $j) % 2 == 0) {
-                        $this->casillas[$i][$j]->cambioOcupada(true, new Pieza($i, $j, 'negro'));
-                    }
+
+                  $this->casillas[$i][$j]->cambioOcupada(true, $this->negras[$i][$j]);
+                  
                 }
             }
             //Negras
 
             for ($i = count($this->casillas) - 1; $i > count($this->casillas) - 4; $i--) {
-                for ($j = 0; $j < count($this->casillas[$i]); $j++) {
-                    if (($i + $j) % 2 == 0) {
-                        $this->casillas[$i][$j]->cambioOcupada(true, new Pieza($i, $j, 'blanco'));
-                    }
+                for ($j = 1; $j < count($this->casillas[$i]); $j+2) {
+                        $this->casillas[$i][$j]->cambioOcupada(true, $this->blancas[$i][$j]);
                 }
             }
         }
@@ -108,23 +147,25 @@
         function moverPieza($posXIni, $posYIni, $posXFin, $posYFin, $colorTurno)
         {
 
-            if (($posXFin == $posXIni + 1 || $posXFin == $posXIni - 1) && ($posYIni == $posYIni + 1)) { //Comprueba el movimiento en la dirección correcta
-                if (isset($this->casillas[$posXFin][$posYFin])) {       //Comprueba que existe la casilla de destino
-                    if ($this->casillas[$posXFin][$posYFin]['ocupada'] == false) { //Comprueba que la casilla de destino no está ocupada
-                        if ($this->casillas[$posXIni][$posYIni]['ocupada'] === true) {                        //Comprueba que la posición inicial tiene una pieza
-                            if ($this->casillas[$posXIni][$posYIni]->getPieza()['color'] === $colorTurno) {       //Si la pieza del color es del turno que toca
-                                $pieza = $this->casillas[$posXIni][$posYIni]->getPieza();
-                                $this->casillas[$posXIni][$posYIni]->cambioOcupada(false, null);
-                                $this->casillas[$posXFin][$posYFin]->cambioOcupada(true, $pieza);
-                            } else {
-                                return false;
-                            }
-                        } else {
-                            return false;
-                        }
-                    }
-                }
-            }
+            $pieza = $this->casillas[$posXIni][$posYIni]->getPieza();
+            $this->casillas[$posXIni][$posYIni]->cambioOcupada(null);
+            $this->casillas[$posXFin][$posYFin]->cambioOcupada($pieza);
+            
+            // if (($posXFin == $posXIni + 1 || $posXFin == $posXIni - 1) && ($posYIni == $posYIni + 1)) { //Comprueba el movimiento en la dirección correcta
+            //     if (isset($this->casillas[$posXFin][$posYFin])) {       //Comprueba que existe la casilla de destino
+            //         if ($this->casillas[$posXFin][$posYFin]['ocupada'] == false) { //Comprueba que la casilla de destino no está ocupada
+            //             if ($this->casillas[$posXIni][$posYIni]['ocupada'] === true) {                        //Comprueba que la posición inicial tiene una pieza
+            //                 if ($this->casillas[$posXIni][$posYIni]->getPieza()['color'] === $colorTurno) {       //Si la pieza del color es del turno que toca
+            //                     
+            //                 } else {
+            //                     return false;
+            //                 }
+            //             } else {
+            //                 return false;
+            //             }
+            //         }
+            //     }
+            // }
         }
 
         function compruebaTablero()
@@ -163,14 +204,15 @@
 
     class Casilla
     {
-        public $ocupada;
-        public $pieza;
-
+        private $ocupada;
+        private $pieza;
+        private $color;
 
         function __construct()
         {
             $this->ocupada = false;
             $this->pieza = null;
+            $this->color = null;
         }
 
         function getOcupada() //Devuelve si esta ocupada
@@ -178,10 +220,15 @@
             return $this->ocupada;
         }
 
-        function cambioOcupada($cambio, $pieza) //Si se mueve una pieza a la posicion hay un cambio de ocupacion
+        function cambioOcupada($pieza) //Si se mueve una pieza a la posicion hay un cambio de ocupacion
         { //Set ocupada
-            $this->ocupada = $cambio;
-            $this->pieza = $pieza;
+            if($pieza != null) {
+                $this->ocupada = true;
+                $this->pieza = $pieza;
+            }else{
+                $this->ocupada = false;
+                $this->pieza = null;
+            }
         }
 
         function getPieza()
@@ -228,6 +275,22 @@
         }
     }
 
+    function muestraTablero($tablero){
+
+        for ($i = 0; $i < count($tablero->getTablero()); $i++) {
+            for ($j = 0; $j < count($tablero->getTablero()[$i]); $j++) {
+                echo "<div>";
+                if ($tablero->getCasilla($i, $j)->getOcupada() === true) {
+                    if (($i + $j) % 2 == 0) {
+                        echo "<img class = \"fichas\" src = " . $tablero->muestraImagen($tablero->getCasilla($i, $j)) . " alt = \"No encontre\">";
+                    }
+                }
+                echo "</div>";
+            }
+            echo "<div class = \"par\"><p> $i </p></div>";
+        }
+    }
+
     if (isset($_GET['enviado'])) {
         $posXIni = $_GET['posXInicial'];
         $posYIni = $_GET['posYInicial'];
@@ -235,15 +298,35 @@
         $posYFinal = $_GET['posYFinal'];
     }
 
+    
+
     $colorDelTurno = 'blanco'; //Empiezan las blancas
 
     $tablero = new Tablero();
     $tablero->construyeTablero();
     $tablero->rellenaTableroDeCasillas();
-    $tablero->rellenaTableroDeFichas();
+    $tablero->crearFichas();
+    echo '<pre>';
+    var_dump($tablero->getPiezas());
+    if (isset($_GET['enviado'])) {
+       // $tablero->moverPieza(5, );
+    }
+    echo '</pre>';
+    //$tablero->rellenaTableroDeFichas()4;
+    if (isset($_GET['enviado'])) {
 
+        //$tablero->moverPieza($posXIni, $posYIni, $posXFinal, $posYFinal, 'blanco');
+        echo $posXIni;
+        echo $posYIni;
+        echo $posXFinal;
+        echo $posYFinal;
+    }
+
+
+    $tablero->muestraImagen($tablero->getCasilla(7,2));
 
     ?>
+    <a href="damas.php">Reset</a>
     <pre>
     <?php
     // echo count($tablero->getTablero());
@@ -253,11 +336,11 @@
 </pre>
     <form action="<?php $_SERVER['PHP_SELF'] ?>" method="get">
         <label for="posInicial">Posicion inicial</label><br>
-        <input type="text" id="posInicial" name="posXInicial" placeholder="posX"><br>
-        <input type="text" name="posYInicial" placeholder="posY"><br><br>
+        <input type="text" id="posInicial" name="posXInicial" autocomplete="off" placeholder="posX"><br>
+        <input type="text" name="posYInicial" autocomplete="off" placeholder="posY"><br><br>
         <label for="posFinal">Posicion final</label><br>
-        <input type="text" id="posFinal" name="posXFinal" placeholder="posX"><br>
-        <input type="text" name="posYFinal" placeholder="posY"><br><br>
+        <input type="text" id="posFinal" name="posXFinal" autocomplete="off" placeholder="posX"><br>
+        <input type="text" name="posYFinal"  autocomplete="off" placeholder="posY"><br><br>
 
         <input type="hidden" name="color" value = "<?php if(isset($_GET['color']))  echo $_GET['color'] ?>" >
 
@@ -266,23 +349,14 @@
     </main>
 
 
-    <main>
+
         <div class="tablero">
             <!-- Aquí van las casillas -->
 
             <?php
 
-            for ($i = 0; $i < count($tablero->getTablero()); $i++) {
-                for ($j = 0; $j < count($tablero->getTablero()[$i]); $j++) {
-                    echo "<div>";
-                    if ($tablero->getCasilla($i, $j)->getOcupada() === true) {
-                        if (($i + $j) % 2 == 0) {
-                            echo "<img class = \"fichas\" src = " . $tablero->muestraImagen($tablero->getCasilla($i, $j)) . " alt = \"No encontre\">";
-                        }
-                    }
-                    echo "</div>";
-                }
-            }
+            muestraTablero($tablero);
+            
             ?>
         </div>
 
